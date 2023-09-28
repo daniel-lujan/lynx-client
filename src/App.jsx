7943f241cea23f00a56e47a7c48ced272856bf51
daniel-lujan <daniel.lujan@udea.edu.co>
2023-09-27 20:44:46 -0500

    feat: :heavy_plus_sign: Setup react query client

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./App.css";
 import UserForm from "./components/user-form";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <UserForm />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <UserForm />
+      </div>
+    </QueryClientProvider>
   );
 }
 
