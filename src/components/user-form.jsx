98804435a7f4e6f1e662a65a71331b19adf3cb2f
daniel-lujan <daniel.lujan@udea.edu.co>
2023-09-29 06:58:33 -0500

    feat: :sparkles: Add steps to user form

--- src/components/user-form.jsx
+++ src/components/user-form.jsx
@@ -4,24 +4,48 @@ import FancyInput from "./fancy-input";
 import { AtSymIcon, Numbers123Icon, UserIcon } from "./icons";
 import GenderSelector from "./gender-selector";
 import useDigits from "../hooks/use-digits";
+import useFormData from "../hooks/use-form-data";
+import RangeSlider from "./range-slider";
 
 function UserForm() {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [gender, setGender] = useState("");
-  const [age, setAge] = useDigits("");
+  const [step, setStep] = useState(0);
 
   return (
     <div className={`container ${styles.container}`}>
+      {step === 0 && <BasicData onContinue={() => setStep(1)} />}
+      {step === 1 && (
+        <Tastes onContinue={() => setStep(2)} onReturn={() => setStep(0)} />
+      )}
+    </div>
+  );
+}
+
+function BasicData({ onContinue }) {
+  function handleContinue() {
+    if (!isValidForm) return;
+    setData({
+      ...data,
+      email,
+      name,
+      gender,
+      age,
+    });
+    onContinue();
+  }
+
+  const { data, setData } = useFormData();
+
+  const [email, setEmail] = useState(data.email ?? "");
+  const [name, setName] = useState(data.name ?? "");
+  const [gender, setGender] = useState(data.gender ?? "");
+  const [age, setAge] = useDigits(data.age ?? "");
+
+  const isValidForm = email && name.trim() && age;
+
+  return (
+    <>
       <h1 className="centered">
-        Responde{" "}
-        <b
-          style={{
-            color: "var(--primary-color)",
-          }}
-        >
-          sin mentir
-        </b>
+        Responde <b className="text-primary">sin mentir</b>
       </h1>
       <p className="text-opaque centered">
         Todo es obligatorio, excepto el género
@@ -31,7 +55,11 @@ function UserForm() {
         type="text"
         placeholder="Correo electrónico"
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={(e) => {
+          if (!/\s/.test(e.target.value)) {
+            setEmail(e.target.value);
+          }
+        }}
         maxLength={50}
       />
       <FancyInput
@@ -51,8 +79,23 @@ function UserForm() {
         maxLength={2}
       />
       <GenderSelector state={gender} setState={setGender} />
-      <button>LISTO!</button>
-    </div>
+      <button onClick={handleContinue} disabled={!isValidForm}>
+        {isValidForm ? "CONTINUAR" : "Completa los campos"}
+      </button>
+    </>
+  );
+}
+
+function Tastes({ onContinue, onReturn }) {
+  const [moviesTaste, setMoviesTaste] = useState(0);
+  console.log(moviesTaste);
+  return (
+    <>
+      <h1 className="centered">
+        Qué tanto <b className="text-primary">te gusta...</b>
+      </h1>
+      <RangeSlider value={moviesTaste} onChange={setMoviesTaste} />
+    </>
   );
 }
 
