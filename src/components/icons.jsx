b8bc597963bcd21a425597e4173ee88241890365
daniel-lujan <daniel.lujan@udea.edu.co>
2023-10-01 15:59:34 -0500

    feat: :sparkles: Add check icon

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -101,3 +101,22 @@ export const Numbers123Icon = (props) => (
     <path d="M17 8h2.5a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1 -1.5 1.5h-1.5h1.5a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1 -1.5 1.5h-2.5"></path>
   </svg>
 );
+
+export const CheckIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon icon-tabler icon-tabler-check"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M5 12l5 5l10 -10"></path>
+  </svg>
+);
