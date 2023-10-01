4b776a25444bbb0c914aab2e45c08c6c870a8a14
daniel-lujan <daniel.lujan@udea.edu.co>
2023-10-01 18:22:37 -0500

    refactor: :heavy_plus_sign: Migrate to axios

--- src/api/fetchers.js
+++ src/api/fetchers.js
@@ -1,6 +1,15 @@
-const API = import.meta.env.VITE_API_URL;
+import axios from "axios";
+
+const fetcher = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 export async function getForm(id) {
-  const res = await fetch(`${API}/form/${id}`);
-  return await res.json();
+  const { data } = await fetcher.get(`/form/${id}`);
+  return data;
+}
+
+export async function postForm(userResponse) {
+  const { data } = await fetcher.post("/form", userResponse);
+  return data;
 }
