555e0c080221c5bd17ae4b86f5c21f93fbd11961
daniel-lujan <daniel.lujan@udea.edu.co>
2023-10-02 11:00:09 -0500

    feat: :sparkles: Add nearest establishment to results

--- src/pages/results.jsx
+++ src/pages/results.jsx
@@ -5,6 +5,10 @@ import AnimatedPage from "../components/animated-page";
 import GlobalLoader from "../components/global-loader";
 import { useNavigate } from "react-router";
 import UserCard from "../components/user-card";
+import EstablishmentCard from "../components/establishment-card";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { FADE } from "../constants/animations";
 
 export default function ResultsPage() {
   const navigate = useNavigate();
@@ -15,15 +19,40 @@ export default function ResultsPage() {
     enabled: Boolean(window.localStorage.getItem(LOCAL_STORAGE_ID)),
   });
 
+  const [showMore, setShowMore] = useState(false);
+
+  if (isLoading) {
+    return <GlobalLoader />;
+  }
+
+  if (!isSuccess) return null;
+
   return (
     <AnimatedPage>
-      {isLoading && <GlobalLoader />}
       <h1 className="centered">Resultados</h1>
       <p>
         Aquí tienes a tu <b className="text-primary">pareja ideal:</b>
       </p>
-      {isSuccess && (
-        <UserCard {...data.bestMatch} perfectMatch={data.perfectMatch} />
+      <UserCard {...data.bestMatch} perfectMatch={data.perfectMatch} />
+      {showMore ? (
+        <motion.div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+          }}
+          variants={FADE}
+          initial="initial"
+          animate="animate"
+        >
+          <p>
+            Y puedes pasar con ella una{" "}
+            <b className="text-primary">noche de pasión</b> en:
+          </p>
+          <EstablishmentCard {...data.nearestEstablishment} />
+        </motion.div>
+      ) : (
+        <button onClick={() => setShowMore(true)}>Mostrar más</button>
       )}
     </AnimatedPage>
   );
