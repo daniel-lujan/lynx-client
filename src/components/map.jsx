057b73a69ea956640fcb4e62ce0c9a818449b212
daniel-lujan <daniel.lujan@udea.edu.co>
2023-10-01 20:08:41 -0500

    feat: :sparkles: Add Google Maps

---
+++ src/components/map.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { useEffect } from "react";
+import { createCustomEqual } from "fast-equals";
+
+export default function Map({ onClick, onIdle, children, style, ...options }) {
+  const ref = React.useRef(null);
+  const [map, setMap] = React.useState();
+
+  React.useEffect(() => {
+    if (ref.current && !map) {
+      setMap(new window.google.maps.Map(ref.current, {}));
+    }
+  }, [ref, map]);
+
+  React.useEffect(() => {
+    if (map) {
+      ["click", "idle"].forEach((eventName) =>
+        window.google.maps.event.clearListeners(map, eventName)
+      );
+
+      if (onClick) {
+        map.addListener("click", onClick);
+      }
+
+      if (onIdle) {
+        map.addListener("idle", () => onIdle(map));
+      }
+    }
+  }, [map, onClick, onIdle]);
+
+  useDeepCompareEffectForMaps(() => {
+    if (map) {
+      map.setOptions(options);
+    }
+  }, [map, options]);
+
+  return (
+    <>
+      <div ref={ref} style={style} />
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          // set the map prop on the child component
+          // @ts-ignore
+          return React.cloneElement(child, { map });
+        }
+      })}
+    </>
+  );
+}
+
+const deepCompareEqualsForMaps = createCustomEqual((deepEqual) => (a, b) => {
+  if (
+    a instanceof window.google.maps.LatLng ||
+    b instanceof window.google.maps.LatLng
+  ) {
+    return new window.google.maps.LatLng(a).equals(
+      new window.google.maps.LatLng(b)
+    );
+  }
+
+  // TODO extend to other types
+
+  // use fast-equals for other objects
+  return deepEqual(a, b);
+});
+
+function useDeepCompareMemoize(value) {
+  const ref = React.useRef();
+
+  if (!deepCompareEqualsForMaps(value, ref.current)) {
+    ref.current = value;
+  }
+
+  return ref.current;
+}
+
+function useDeepCompareEffectForMaps(callback, dependencies) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(callback, dependencies.map(useDeepCompareMemoize));
+}
+
+export function Marker(options) {
+  const [marker, setMarker] = React.useState();
+
+  React.useEffect(() => {
+    if (!marker) {
+      setMarker(new window.google.maps.Marker());
+    }
+
+    // remove marker from map on unmount
+    return () => {
+      if (marker) {
+        marker.setMap(null);
+      }
+    };
+  }, [marker]);
+  React.useEffect(() => {
+    if (marker) {
+      marker.setOptions(options);
+    }
+  }, [marker, options]);
+  return null;
+}
+
+export function MapContainer({ children }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "400px",
+        width: "400px",
+        maxWidth: "100%",
+        borderRadius: "16px",
+        overflow: "hidden",
+        alignSelf: "center",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
